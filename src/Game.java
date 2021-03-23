ebbd2873e4e277425fd38bf0ac110183ee0a4a3c
sedx876 <swatkins876@gmail.com>
2021-03-23 05:48:13 -0400

    initial commit from local machine

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	//GameLoop
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Window.java
@@ -0,0 +1,13 @@
+
+public class Window {
+	
+	public Window(int width, int height, String title, Game game) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
